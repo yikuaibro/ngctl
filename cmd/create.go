430c99f08eb7c9ecf3b6b45abb294779eda2e79d
li <1312513963@qq.com>
2023-05-28 10:46:59 +0800

    feat: add namespace flag for create command

--- cmd/create.go
+++ cmd/create.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
@@ -50,14 +51,17 @@ func NewCreateCommand(c argclient.Args, ioStream util.IOStreams) *cobra.Command 
 				return err
 			}
 			ng.client = newClient
+			if err = ng.getNamespaceArg(ctx, cmd); err != nil {
+				return err
+			}
 			if err = ng.createService(ctx, types.NamespacedName{
-				Namespace: util.DefaultNamespace,
+				Namespace: ng.Namespace,
 				Name:      service.DefaultServiceName,
 			}); err != nil {
 				return err
 			}
 			if err = ng.createDeployment(ctx, types.NamespacedName{
-				Namespace: util.DefaultNamespace,
+				Namespace: ng.Namespace,
 				Name:      deployment.DefaultDeploymentName,
 			}); err != nil {
 				return err
@@ -69,8 +73,28 @@ func NewCreateCommand(c argclient.Args, ioStream util.IOStreams) *cobra.Command 
 		},
 	}
 	cmd.Flags().BoolP("local", "l", false, "local access to nebula-studio")
+	cmd.Flags().StringP("namespace", "n", util.DefaultNamespace, "namespace of nebula-studio")
 	cmd.SetOut(ioStream.Out)
 	return cmd
+}
+
+func (ng *ngCreateOptions) getNamespaceArg(ctx context.Context, cmd *cobra.Command) error {
+	nn, _ := cmd.Flags().GetString("namespace")
+	namespace := &corev1.Namespace{}
+	typenn := types.NamespacedName{Namespace: nn}
+	if err := ng.client.Get(ctx, typenn, namespace); err != nil {
+		if apierrors.IsNotFound(err) {
+			klog.Infof("%s namespace not found, ready to create", nn)
+			namespace.Name = nn
+			if err = ng.client.Create(ctx, namespace); err != nil {
+				return err
+			}
+			ng.Namespace = nn
+		} else {
+			return err
+		}
+	}
+	return nil
 }
 
 func (ng *ngCreateOptions) createService(ctx context.Context, nn types.NamespacedName) error {
@@ -81,6 +105,7 @@ func (ng *ngCreateOptions) createService(ctx context.Context, nn types.Namespace
 	}
 	if svc == nil {
 		svc = service.DefaultService()
+		klog.Infof("create service, Namespace: %s, Name: %s", svc.Namespace, svc.Name)
 		if err = svcClient.Create(ctx, svc); err != nil {
 			return err
 		}
